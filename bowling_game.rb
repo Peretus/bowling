f545d91dec8ba2b52fee0828e9c84eb197da798a
Peretus <caseymcneil@gmail.com>
2013-11-21 12:08:24 -0600

    Remove inject method from #score and build better scoring system to take into account spares. DRY up the _spec file by adding let method.

--- bowling_game.rb
+++ bowling_game.rb
@@ -20,10 +20,23 @@ class BowlingGame
 
 	# Returns as an Integer the total of all the rolls in the @rolls array.
 	def score
-		@rolls.inject(0) {|result, number| result+number}
+		# @rolls.inject(0) {|result, number| result+number}
+		total_score = 0
+		current_roll = 0
+
+		while current_roll < @rolls.size
+			roll 		= @rolls[current_roll]
+			next_roll 	= @rolls[current_roll+1]
+
+			if roll + next_roll == 10
+				total_score += 10+@rolls[current_roll+2]
+			else 
+				total_score += roll + next_roll
+			end
+
+			current_roll += 2
+		end
+
+		return total_score
 	end
-
-	
-
-
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -14,16 +14,30 @@ describe BowlingGame do
 	end
 
 	describe "#score" do
-		it "should set the score to 0 when we have all gutterballs" do
-			20.times {game.roll(0)}
+		context "when we have all gutterballs..." do
+			it "should set the score to 0:" do
+				20.times {game.roll(0)}
 
-			expect(game.score).to eq 0
+				expect(game.score).to eq 0
+			end
 		end
 
-		it "should set the score as 20 when one pin is knocked down on each roll" do 
-			20.times { game.roll(1) }
+		context "When one pin is knocked down on each roll..." do
+		
+			it "should set the score as 20:" do 
+				20.times { game.roll(1) }
 
-			expect(game.score).to eq 20
+				expect(game.score).to eq 20
+			end
+		end
+
+		context "When a spare is achieved..." do
+			it "then the score should be 10 plus the number of pins knocked down on the next roll." do
+				3.times {game.roll(5)}
+				17.times {game.roll(0)}
+
+				expect(game.score).to eq 20
+			end
 		end
 	end
 end
