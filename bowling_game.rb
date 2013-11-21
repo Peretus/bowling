4838939aef40d8617a499ec78b64f8be54096e2d
Peretus <caseymcneil@gmail.com>
2013-11-21 11:29:34 -0600

    Add code to bowling_game to add each pin into the @score array.  Add code to return the sum of all the pins in the @score array.

--- bowling_game.rb
+++ bowling_game.rb
@@ -5,13 +5,25 @@ class BowlingGame
 		@rolls = []
 	end
 
+	# Record a roll in the BowlingGame
+
+	# pins - An Integer of pins that are knocked down in each roll.
+
+	# Returns nothing
 	def roll(pins)
-		# @rolls << pins
+		@rolls << pins
 	end
 
+
+	# Returns the total score of all the rolls. Expects to be run
+	# at the end of the game.
+
+	# Returns as an Integer the total of all the rolls in the @rolls array.
 	def score
-		0
+		@rolls.inject(0) {|result, number| result+number}
 	end
+
+	
 
 
 end
